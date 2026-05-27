76a7c3b2fcabf5805b17e700e747cb251653d11e
jpcompetente <jp.competente@outlook.com>
2026-05-27 14:09:04 +0800

    waa

--- frontend-vite/src/components/OperatorDashboard.jsx
+++ frontend-vite/src/components/OperatorDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+//import React, { useEffect, useRef, useState } from "react";
 import Webcam from "react-webcam";
 import { detectImage, getOperatorPreset } from "../api/backend";
 import "../styles/operator-dashboard.css";
